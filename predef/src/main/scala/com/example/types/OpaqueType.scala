4c30bb44d603318bcb90b182db53cdbc14e1a024
Matt Dziuban <mrdziuban@gmail.com>
2025-09-22 09:41:50 -0400

    Use `Ordering` instead of `Numeric`.

--- predef/src/main/scala/com/example/types/OpaqueType.scala
+++ predef/src/main/scala/com/example/types/OpaqueType.scala
@@ -2,5 +2,5 @@ package com.example.types
 
 opaque type OpaqueType[A] = Int
 object OpaqueType {
-  given numeric[A]: Numeric[OpaqueType[A]] = Numeric[Int]
+  given ordering[A]: Ordering[OpaqueType[A]] = Ordering[Int]
 }
--- usage/src/main/scala/com/example/Foo.scala
+++ usage/src/main/scala/com/example/Foo.scala
@@ -2,7 +2,7 @@ package com.example
 
 case class Foo[F[_]](id: F[Int])
 object Foo {
-  given numeric[F[_]](using o: Numeric[F[Int]]): Numeric[Foo[F]] = ???
+  given ordering[F[_]](using o: Ordering[F[Int]]): Ordering[Foo[F]] = ???
 }
 
-val fooNumeric = Numeric[Foo[OpaqueType]]
+val fooOrdering = Ordering[Foo[OpaqueType]]
